eadaa4379818a7306e482264c7a5120108e1cd2e
i-i-ijul <haqqikamvret@gmail.com>
2025-06-02 14:51:11 +0700

    mendeklarasikan function saklar pada int main

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -15,4 +15,16 @@ class remoteLampu{
 
 int main(){
     remoteLampu lampuRumah;
-}
+
+    lampuRumah.setSaklarNo(0, "lampu teras rumah");
+    lampuRumah.setSaklarNo(1, "lampu ruang tamu");
+    lampuRumah.setSaklarNo(2, "lampu kamar tidur");
+    lampuRumah.setSaklarNo(3, "lampu dapur");
+
+    cout << lampuRumah.getSaklarNo(0) << "lampu teras rumah" << endl;
+    cout << lampuRumah.getSaklarNo(1) << "lampu ruang tamu" << endl;
+    cout << lampuRumah.getSaklarNo(2) << "lampu kamar tidur" << endl;
+    cout << lampuRumah.getSaklarNo(3) << "lampu dapur" << endl;
+
+    return 0;
+};
