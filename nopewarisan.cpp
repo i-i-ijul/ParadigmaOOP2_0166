7179c8bee86b1ee52a9ae96d262d327ffdca28a0
i-i-ijul <haqqikamvret@gmail.com>
2025-06-02 15:03:06 +0700

    menambahkan final class

---
+++ nopewarisan.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class baseClass final{
+    public:
+    virtual void perkenalan(){
+        cout << "hallo saya ijul dari brebes";
+    }
+};
+
+class derivedClass : public baseClass{ //gabisa dipanggil karena si baseclass udah final
+    public:
+        void perkenalan(){
+            cout << "hallo saya ilham dari juragan empang";
+        }
+};
+
+int main(){
+    
+}
