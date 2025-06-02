5ebc68f593ebd3873a3dd36c0b43bf7e654a6ce7
i-i-ijul <haqqikamvret@gmail.com>
2025-06-02 14:18:48 +0700

    menambahkan class seseorang

---
+++ polimorpi.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+class seseorang{
+    public:
+    virtual void pesan(){
+        cout << "chat from someone" << endl;
+    }
+};
+
+int main(){
+    
+}
