61dc454f0e02680407e1784f56ab2a1c4796ac5f
i-i-ijul <haqqikamvret@gmail.com>
2025-06-02 15:06:04 +0700

    memanggil virtual void final

---
+++ noOverriding.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class baseClass final{
+    public:
+    virtual void perkenalan()final{
+        cout << "hallo saya ijul dari brebes";
+    }
+};
+
+class derivedClass : public baseClass{ //gabisa dipanggil karena si baseclass udah final
+    public:
+        void perkenalan(){
+            cout << "hallo saya ilham dari juragan empang";
+        }
+};
+
+int main(){
+    derivedClass a;
+    a.perkenalan();
+
+    return 0;
+}
